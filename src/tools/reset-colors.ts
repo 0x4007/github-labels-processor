6fe8abdc87a96366e4487597631d56264bb029ce
アレクサンダー.eth <gpg@pavlovcik.com>
2025-01-02 11:20:15 +0800

    fix: resetColors

--- src/tools/reset-colors.ts
+++ src/tools/reset-colors.ts
@@ -1,20 +1,21 @@
+import { ARGS } from "../cli/cli-args";
 import filterLabels from "../utils/filter-labels";
 import getLabels from "../utils/get-labels";
 import colorizeLabels from "./colorize-labels";
 
-export default async function resetColors(args: { color?: string; owner: string; repository: string }) {
+export default async function resetColors() {
   // Get all labels.
   const allLabels = await getLabels();
-  // do not select labels with `^Price:.+` regex
-  const notPriceLabels = await filterLabels(allLabels, `^(?!Price:.+).+`);
 
-  // Default to grey color.
-  args.color = "ededed";
-  await colorizeLabels(notPriceLabels);
-
-  // Filter for `Price: ` labels.
+  // Filter for `Price: ` labels first
   const priceLabels = await filterLabels(allLabels, `^Price:.+`);
   // Set `Price: ` labels to green.
-  args.color = "1f883d";
+  ARGS.color = "1f883d";
   await colorizeLabels(priceLabels);
+
+  // Get all non-price labels (including Priority and Time labels)
+  const notPriceLabels = await filterLabels(allLabels, `^(?!Price:.+).+`);
+  // Set to grey color.
+  ARGS.color = "ededed";
+  await colorizeLabels(notPriceLabels);
 }
